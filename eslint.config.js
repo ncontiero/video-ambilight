be5efecef6893f820c6eac07292cfefa6b54fd49
Nicolas Contiero <git@ncontiero.com>
2026-05-13 20:19:13 -0300

    chore: relax tailwind class linting and adjust body class order

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,8 @@ import { ncontiero } from "@ncontiero/eslint-config";
 export default ncontiero({
   tailwindcss: {
     cssGlobalPath: "./styles/input.css",
+    overrides: {
+      "tailwindcss/no-unknown-classes": ["warn", { ignore: ["github-button"] }],
+    },
   },
 });
